034c9a9c4efcaad1c7b9df3fa2704939434b9fa7
Noah Pope <nlpope@tutanota.com>
2020-07-20 20:13:49 -0400

    got images and desc to display

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 const KEY = "AIzaSyCWbKmmqB4N_zqqTFjajXG7AYivQ0SNkXk";
 
 class App extends React.Component {
-  onTermSubmit = (term) => {
-    youtube.get("/search", {
+  state = {
+    videos: [],
+  };
+  onTermSubmit = async (term) => {
+    const response = await youtube.get("/search", {
       params: {
         q: term,
         part: "snippet",
@@ -14,12 +18,14 @@ class App extends React.Component {
         key: KEY,
       },
     });
+    this.setState({ videos: response.data.items });
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onTermSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
